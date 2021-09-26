35b03a442c053084bd706d9e26d96fbc7d347422
Andrey Sushkov <>
2021-09-27 02:48:50 +0300

    Add markings for items

--- ShoppingList/Modules/Main/CustomTableViewCell.swift
+++ ShoppingList/Modules/Main/CustomTableViewCell.swift
@@ -9,4 +9,14 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
     @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var greenMark: UIImageView!
+    
+    @IBAction func cartButtonTapped(_ sender: Any) {
+        if greenMark.isHidden {
+            greenMark.isHidden = false
+        } else {
+            greenMark.isHidden = true
+        }
+    }
+    
 }
